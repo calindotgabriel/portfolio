cacf0cee2660676cd20cb9cd3d231c07339309f4
Calin Gabriel <gabriel.calin_ext@scout24.com>
2025-08-20 09:27:31 +0300

    test deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://calindotgabriel.github.io",
-  base: "/portfolio",
+  base: "/portfolio", // repo name
   output: "static",
 });
