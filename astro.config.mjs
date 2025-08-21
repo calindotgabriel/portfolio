38f6c86d2507f22742d1da5d71c517d2641dcc45
Calin Gabriel <gabriel.calin_ext@scout24.com>
2025-08-21 10:52:28 +0300

    sitemap to public folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://calindotgabriel.github.io",
-  base: "/portfolio", // repo name
   output: "static",
+  build: {
+    assets: "assets",
+  },
 });
